8747152356a8a9b15fd3e8de3c20cd729cf753b0
hekaahve <heini.k.ahven@student.jyu.fi>
2020-12-09 18:33:53 +0200

    Add project files.

---
+++ WebApplication2/About.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Title="About" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="About.aspx.cs" Inherits="WebApplication2.About" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <h2><%: Title %>.</h2>
+    <h3>Your application description page.</h3>
+    <p>Use this area to provide additional information.</p>
+    jou
+
+    <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+    Tähän täytyy saada SQL tietokanta, joka hakee tiedot
+    Koirarekisteri? hakee koirien tiedot
+    <asp:Label ID="Label1" runat="server" Text="0"></asp:Label> 
+    <asp:Login ID="Login1" runat="server"></asp:Login>
+    <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Button" />
+    <asp:TextBox ID="hakukentta" runat="server"></asp:TextBox>
+</asp:Content>
---
+++ WebApplication2/About.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication2
+{
+    public partial class About : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int i = Int32.Parse(Label1.Text) + 1;
+            Label1.Text = i.ToString();//muuta lisäämään asioita
+        }
+        /// <summary>
+        /// Hakee hakukentästä asioita. Tässä tapauksessa ottaisi hakukentästä syötetyn
+        /// hakulausekkeen ja hakisi tietokannasta halutut asiat kuten 
+        /// koirien tiedot tms
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            string haku = hakukentta.Text;
+
+        }
+    }
+}
