adcb6e3105835a403cf10e35f293744024c882d6
hekaahve <heini.k.ahven@student.jyu.fi>
2020-12-15 21:31:29 +0200

    Lisätty taulut, tietokantayhteys ja controlleri Koirat-projektiin. Jatka controllerin get-metodia https://www.youtube.com/watch?v=nMGlaiNBbNU&ab_channel=kudvenkat kohdasta 8:45

---
+++ Koirat1/Controllers/KoiratController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using KoiratDataAccess;
+
+namespace Koirat1.Controllers
+{
+    public class KoiratController : ApiController
+    {
+        public IEnumerable<Koirat> Get()
+        {
+            using(KoiratDBEntities entities = new KoiratDBEntities())
+            {
+                return entities.Koirats.ToList();
+                //Jatka https://www.youtube.com/watch?v=nMGlaiNBbNU&ab_channel=kudvenkat kohdasta 8:45
+            }
+        }
+    }
+}
---
+++ Koirat1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Koirat1.WebApiApplication" Language="C#" %>
